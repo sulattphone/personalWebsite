17121467f907cd828cb9997112f564f382d17625
Su Latt Phone <sulattphone@gmail.com>
2020-09-01 01:19:03 -0700

    Got the carousel on home page up

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './routes/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
     return(
--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,14 +1,61 @@
 import React, { Component } from 'react';
-import About from '../components/About';
-import Education from '../components/Education';
+import '../index.css';
+
 
 class Home extends Component {
     render() {
         return(
-            <div>
-                Home New Working
-                <About />
-                <Education />
+            <div className="page">
+                <div id="homePage">
+                <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="carousel-img-div"></div>
+      <div class="carousel-caption d-block">
+      <img src="https://drive.google.com/uc?/export=view&id=18uw8P8-QkSmDI-Dh5e03dv5O1dWe1y-d" class="d-block home-page-img" alt="..." />
+        <h5>Su Latt Phone</h5>
+        <div className="carousel-subTitle-div">
+        <p>Hi I’m Su Latt. Welcome! Whoever you are, I hope you get something out of visiting my website!</p>
+        </div>
+        
+      </div>
+    </div>
+    <div class="carousel-item">
+    <div class="carousel-img-div"></div>
+      <div class="carousel-caption d-block">
+      <img src="https://drive.google.com/uc?/export=view&id=18uw8P8-QkSmDI-Dh5e03dv5O1dWe1y-d" class="d-block home-page-img" alt="..." />
+        <h5>About Me</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+    <div class="carousel-img-div"></div>
+      <div class="carousel-caption d-block">
+      <img src="https://drive.google.com/uc?/export=view&id=18uw8P8-QkSmDI-Dh5e03dv5O1dWe1y-d" class="d-block home-page-img" alt="..." />
+        <h5>Experience</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>              
+                </div>
+                <div className="card-img-overlay" id="darkOverlay">
+
+                </div>
+                
             </div>
         );
     }
